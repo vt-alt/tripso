vt-alt/tripso

commit 81c6f86090cb2399fb78ca84c1268628e4d6d77b

Allow to build with system CFLAGS

Makefile
@@ -7,6 +7,7 @@ KVER	?= $(shell uname -r)
 KDIR	?= /lib/modules/$(KVER)/build/
 DEPMOD	= /sbin/depmod -a
 CC	?= gcc
+CFLAGS	?= -O2 -g
 XFLAGS	?= $(shell pkg-config xtables --cflags 2>/dev/null)
 XDIR	?= $(shell pkg-config --variable xtlibdir xtables)
 VERSION	= $(shell git -C $M describe --dirty)
@@ -31,7 +32,7 @@ install-lib: libxt_TRIPSO.so
 	gcc -shared -o $@ $< $(shell pkg-config xtables --libs)
 
 %_sh.o: libxt_TRIPSO.c xt_TRIPSO.h
-	gcc -O2 -Wall -Wunused -fPIC ${XFLAGS} ${CFLAGS} -o $@ -c $<
+	gcc -Wall -Wunused -fPIC $(XFLAGS) $(CFLAGS) -o $@ -c $<
 
 clean:
 	-make -C $(KDIR) M=$(CURDIR) clean

commit a1c1327aa7359c1827d5e2439bc342fd5e9489eb

Fix building of kernel module
scripts/Makefile.build:50: *** CFLAGS was changed in "/usr/src/RPM/BUILD/tripso-1.0/Makefile". Fix it to use ccflags-y.  Stop.

Fixes: 81c6f86 ("Allow to build with system CFLAGS")

Makefile
@@ -7,7 +7,9 @@ KVER	?= $(shell uname -r)
 KDIR	?= /lib/modules/$(KVER)/build/
 DEPMOD	= /sbin/depmod -a
 CC	?= gcc
+ifeq ($(KERNELRELEASE),)
 CFLAGS	?= -O2 -g
+endif
 XFLAGS	?= $(shell pkg-config xtables --cflags 2>/dev/null)
 XDIR	?= $(shell pkg-config --variable xtlibdir xtables)
 VERSION	= $(shell git -C $M describe --dirty)