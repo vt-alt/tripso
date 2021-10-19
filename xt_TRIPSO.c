vt-alt/tripso

commit 0f6f1c9a037e77e9955854a921ac6c3c8be3895f

Compatibility drop v3.19, add v5.3
e21951212f03b8d805795d8f71206853b2ab344d skb_ensure_writable is added
2cf6bffc49dae26edd12af6b57c8c780590380bf skb_make_writable is removed

xt_TRIPSO.c
@@ -440,7 +440,7 @@ tripso_tg(struct sk_buff *skb, const struct xt_action_param *par)
 	}
 
 	/* will rewrite whole options set */
-	if (!skb_make_writable(skb, ip_hdrlen(skb)) ||
+	if (!skb_ensure_writable(skb, ip_hdrlen(skb)) ||
 	    !mangle_options(skb, topt, ti))
 		return NF_DROP;
 

commit 30520970658316da6fd635be92c660bda78759b6

Fix unintended dropping of to be mangled packets
0f6f1c9 ("Compatibility drop v3.19, add v5.3") ports incorrectly from
skb_make_writable to skb_ensure_writable missing that return value is
changed too, causing packet drops instead of mangling.

Reported-by: https://github.com/kernelplv (Michael Mosolov) kpl <kernelplv@gmail.com>
Link: https://github.com/vt-alt/tripso/pull/2
Fixes: 0f6f1c9 ("Compatibility drop v3.19, add v5.3")

xt_TRIPSO.c
@@ -440,7 +440,7 @@ tripso_tg(struct sk_buff *skb, const struct xt_action_param *par)
 	}
 
 	/* will rewrite whole options set */
-	if (!skb_ensure_writable(skb, ip_hdrlen(skb)) ||
+	if (skb_ensure_writable(skb, ip_hdrlen(skb)) ||
 	    !mangle_options(skb, topt, ti))
 		return NF_DROP;
 