4c1f39d4b9734d2dfd1a941d1e9c9a54631d5de1
Vitaly Chikunov <vt@altlinux.org>
2021-10-19 04:43:27 +0300

    Improve testing script
    Output dumps are artificially generated from a single successful run.

--- tripso_tests.sh
+++ tripso_tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
+#
+# PreReq: apt-get install tcpdump tcpreplay iptables net-tools
 
 PATH=$PATH:/sbin:/usr/sbin
 
-se_prermisive() {
-	if getenforce | grep -q Enforcing; then
+se_permissive() {
+	if [ -x /usr/sbin/getenforce ] && getenforce | grep -q Enforcing; then
 		setenforce 0
 		echo Selinux set to: `getenforce`
 	fi
@@ -24,7 +26,7 @@ check_harness() {
 		exit 1
 	fi
 }
-unload_rules_x() {
+unload_rules() {
 	for t in raw mangle nat filter security; do
 		iptables-save -t $t | grep -q TRIPSO || continue
 		iptables-save -t $t | grep TRIPSO \
@@ -38,8 +40,8 @@ netns_exec_fn() {
 	ip netns exec test bash -c "$(declare -f $1); $1"
 }
 unload_module() {
-	netns_exec_fn unload_rules_x
-	unload_rules_x
+	netns_exec_fn unload_rules
+	unload_rules
 	lsmod | grep -q xt_TRIPSO && rmmod -v xt_TRIPSO
 }
 veth_start() {
@@ -53,7 +55,7 @@ veth_start() {
 }
 veth_stop() {
 	ip link show veth0 >/dev/null 2>&1 && ip link del veth0
-	ip netns del test
+	ip netns del test >/dev/null 2>&1
 }
 load_module() {
 	sysctl kernel.printk=8
@@ -66,11 +68,14 @@ load_module() {
 		exit 1
 	fi
 }
-load_rules_x() {
-	iptables -t security -F
-	iptables -t security -A INPUT  -s 10.99.0.1 -j TRIPSO --to-astra
-	iptables -t security -A INPUT  -s 10.99.0.2 -j TRIPSO --to-cipso
-	iptables -t security -A INPUT  -j NFLOG --nflog-group 1
+load_rules_ns() {
+	# security/INPUT does not receive test CIPSO packets, raw/PREROUTING
+	# does. But this is purely artificial, since we don't need to translate
+	# input packets from CIPSO.
+	iptables -t raw -F
+	iptables -t raw -A PREROUTING  -s 10.99.0.1 -j TRIPSO --to-astra
+	iptables -t raw -A PREROUTING  -s 10.99.0.2 -j TRIPSO --to-cipso
+	iptables -t raw -A PREROUTING  -j NFLOG --nflog-group 1
 
 	#iptables -t filter -F
 	#iptables -t filter -A INPUT       -j NFLOG --nflog-group 1
@@ -79,7 +84,7 @@ load_rules() {
 	# tcpreplay'ed outgoing packets not visible to iptables
 	# but they will be visible on the other side of veth, in veth1
 	# in PREROUTING and INPUT chains.
-	netns_exec_fn load_rules_x
+	netns_exec_fn load_rules_ns
 	iptables -t security -F
 	iptables -t security         -A INPUT  -j TRIPSO --to-cipso
 	iptables -t security ! -o lo -A OUTPUT -j TRIPSO --to-astra
@@ -107,20 +112,30 @@ send_pkt() {
 	mkdir -p recv
 	show_dmesg
 	for i in "$@"; do
+		echo
 		j=recv/`basename $i`
 		ip netns exec test \
-			tcpdump -Z root -qnnp -i nflog:1 -c 1 -w $j >/dev/null 2>&1 &
+			tcpdump -Z root -qnnp -i nflog:1 -c 1 -w $j -y IPV4 icmp >/dev/null 2>&1 &
 		TPID=$!
 		sleep 0.1
+		echo = send $i
 		tcpreplay -i veth0 $i > /dev/null
+		tcpdump -tv -nnp -r $i 2>/dev/null | tee a
 		wait $TPID
-		echo = sent
-		tcpdump -tvx -nnp -r $i 2>/dev/null | tee a
 		echo = received
-		tcpdump -tvx -nnp -r $j 2>/dev/null | tee b
-		#echo $i `md5sum a b` >> test-vec.txt
+		tcpdump -tv -nnp -r $j 2>/dev/null | tee b
 		show_dmesg
-
+		if [ -e test-$j ]; then
+			tcpdump -tvxnn -r test-$j > .expected 2>/dev/null
+			tcpdump -tvxnn -r      $j > .received 2>/dev/null
+			if diff -u .expected .received; then
+				echo "= Received packet matches expected!"
+			else
+				echo "! Received packet differs from expected!"
+				exit 1
+			fi
+		fi
+		echo
 	done
 }
 test_send() {
@@ -135,7 +150,7 @@ retest() {
 }
 
 check_harness
-se_prermisive
+se_permissive
 for j; do
 	case $j in
 		restart) restart ;;
